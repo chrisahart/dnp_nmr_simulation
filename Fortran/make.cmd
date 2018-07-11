2797fd18afb6ac6a4946b1f3cb2176a326eaace2
chrisahart <chrissahart@gmail.com>
2018-07-11 13:56:05 +0100

    Optimising Fortran code

---
+++ Fortran/make.cmd
@@ -0,0 +1,9 @@
+gfortran -g -o solid_effect expokit.o mataid.o f2py_functions.f90 f2py_dynamics.f90 solid_effect_main.f90 -lgomp -llapack -lblas -Wall 
+
+solid_effect.exe
+
+::rm f2py_functions.mod
+::rm f2py_dynamics.mod
+::rm solid_effect.exe
+
+PAUSE
